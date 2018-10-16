ff5aa2b5af62aa255023744fa8af8dd4d9f0919f
nazir92 <nazir.alicanov@gmail.com>
2018-10-17 00:48:00 +0400

    solution for VovaAndTrain problem

--- CodeForces/src/codeforces/VovaAndTrain.java
+++ CodeForces/src/codeforces/VovaAndTrain.java
@@ -16,18 +16,26 @@ public class VovaAndTrain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int queries = scanner.nextInt();
-
+        int[] canSeeLanterns = new int[queries];
         int[][] destinations = new int[queries][4];
         for (int i = 0; i < queries; i++) {
             for (int j = 0; j < 4; j++) {
                 destinations[i][j] = scanner.nextInt();
             }
+
+            int lanternsCount = destinations[i][0] / destinations[i][1];
+
+            int v = destinations[i][1];
+            lanternsCount -= (destinations[i][3] / v) - (destinations[i][2] / v);
+            if (destinations[i][2] % v == 0) {
+                lanternsCount--;
+            }
+            canSeeLanterns[i] = lanternsCount;
         }
 
-
-        
-        
-        
+        for (int i = 0; i < queries; i++) {
+            System.out.println(canSeeLanterns[i]);
+        }
 
     }
 }
