a827cd1be2d0690eecf5dc90ac9e15cd3fe441c8
nazir <nazir.alicanov@gmail.com>
2020-02-05 21:53:45 +0400

    new commit

---
+++ CodeForces/src/codeforces/StringTyping.java
@@ -0,0 +1,35 @@
+package codeforces;
+
+import java.util.Scanner;
+// https://codeforces.com/contest/954/problem/B
+public class StringTyping {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lengthOfString = scanner.nextInt();
+        String values = scanner.next();
+
+        int index = lengthOfString / 2;
+        int operations=lengthOfString;
+        if (lengthOfString>1){
+            while (index >= 2) {
+                if (values.substring(0, index).equals(values.substring(index, index * 2))) {
+                    break;
+                } else {
+                    index--;
+                }
+            }
+            operations = index + 1 + lengthOfString - (index * 2);
+        }
+
+
+
+        System.out.println(operations);
+
+
+
+
+
+    }
+}
